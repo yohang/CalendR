c49d992ac83dc0b10510f1e2cd7addef520edb26
yohang <yohan@giarelli.org>
2012-01-20 19:06:14 +0100

    Added concrete Event class

---
+++ src/CalendR/Event/Event.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace CalendR\Event;
+
+/**
+ * Concrete implementation of AbstractEvent and in fact EventInterface.
+ *
+ * In most case, you'd better to implement your own events
+ */
+class Event extends AbstractEvent
+{
+    /**
+     * @var \DateTime
+     */
+    protected $begin;
+
+    /**
+     * @var \DateTime
+     */
+    protected $end;
+
+    /**
+     * @var string
+     */
+    protected $uid;
+
+    public function __construct($uid, \DateTime $start, \DateTime $end)
+    {
+        if ($start->diff($end)->invert == 1) {
+            throw new \InvalidArgumentException('Events usually start before they end');
+        }
+
+        $this->uid = $uid;
+        $this->begin = clone $start;
+        $this->end = clone $end;
+    }
+
+    /**
+     * Returns an unique identifier for the Event.
+     * Could be any string, but MUST to be unique.
+     *   ex : 'event-8', 'meeting-43'
+     *
+     * @return string an unique event identifier
+     */
+    public function getUid()
+    {
+        return $this->uid;
+    }
+
+    /**
+     * Returns the event begin
+     * @return \DateTime event begin
+     */
+    public function getBegin()
+    {
+        return $this->begin;
+    }
+
+    /**
+     * Returns the event end
+     * @return \DateTime event end
+     */
+    public function getEnd()
+    {
+        return $this->end;
+    }
+}
