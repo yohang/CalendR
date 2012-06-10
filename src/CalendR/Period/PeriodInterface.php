dc52b535f20349c88c2a050037a6ab5495fb66b3
Yohan G <yohan@giarelli.org>
2012-06-10 16:26:37 +0200

    Added license un PeriodInterface

--- src/CalendR/Period/PeriodInterface.php
+++ src/CalendR/Period/PeriodInterface.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of CalendR, a Fréquence web project.
+ *
+ * (c) 2012 Fréquence web
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace CalendR\Period;
 
 interface PeriodInterface
