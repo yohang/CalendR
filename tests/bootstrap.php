32778fa0e2920f4e50fcca0876268b8529b41e04
yohang <yohan@giarelli.org>
2012-07-16 17:44:14 +0200

    Added dev dependencies (twig) and updated build.xml

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,18 +1,3 @@
 <?php
 
-spl_autoload_register(function($class)
-{
-    if (0 === strpos($class, 'CalendR\\Test\\')) {
-        $file = __DIR__ . '/' . str_replace('\\', '/', $class) . '.php';
-        if (file_exists($file)) {
-            require_once $file;
-            return true;
-        }
-    } elseif (0 === strpos($class, 'CalendR\\')) {
-        $file = __DIR__ . '/../src/' . str_replace('\\', '/', $class) . '.php';
-        if (file_exists($file)) {
-            require_once $file;
-            return true;
-        }
-    }
-});
+require_once __DIR__.'/../vendor/autoload.php';
